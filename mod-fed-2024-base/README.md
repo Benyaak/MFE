93f71a0eda2ff51c0dd78c17efab359282bdfc1c
Andrei Dan <ghinea.danandrei@gmail.com>
2024-03-19 10:15:00 +0200

    Sync repos

---
+++ mod-fed-2024-base/README.md
@@ -0,0 +1,16 @@
+# Module Federation Tutorial using @angular-architects/module-federation
+
+# Starting Points
+
+main branch - Starting point
+feature/exercise-1 - Starting point for exercise 2, working example of exercise 1
+feature/exercise-2 - Starting point for exercise 3, working example of exercise 2
+feature/exercise-3 - Starting point for bonus exercise, working example of exercise 3
+
+The Micro Frontend playbook powerpoint is available at the root of the repo
+
+Reference Architectures for Module Federation: https://github.com/module-federation/module-federation-examples
+
+
+# Workshop Exercises:
+Available at [https://www.mfeworkshop.com](https://www.mfeworkshop.com)
---
+++ ngrx-starter/projects/angular-ngrx-material-starter/src/app/features/examples/elements/elements.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+import { ROUTE_ANIMATIONS_ELEMENTS } from '../../../core/animations/route.animations';
+
+@Component({
+  selector: 'anms-elements',
+  templateUrl: './elements.component.html',
+  styleUrls: ['./elements.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ElementsComponent implements OnInit {
+  routeAnimationsElements = ROUTE_ANIMATIONS_ELEMENTS;
+  count$ = new BehaviorSubject(0);
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  increment() {
+    const current = this.count$.getValue() + 1;
+    this.count$.next(current);
+  }
+}
